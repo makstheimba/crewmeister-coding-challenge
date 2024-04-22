92576faed8d595dc638d1d999f4b4c473b7a7110
makstheimba <makstheimba@gmail.com>
2024-04-22 10:16:50 +0200

    add allow all cors policy on lambdas

--- api/lambdas/absences.js
+++ api/lambdas/absences.js
@@ -22,7 +22,8 @@ export const handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         },
         body: JSON.stringify({
           "message": "Success",
